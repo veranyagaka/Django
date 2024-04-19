6c3534c942769295dbe01858e20b97a6914eba64
veranyagaka <nyagakaver@gmail.com>
2024-04-20 00:03:43 +0300

    Integrated Database

--- VERA/urls.py
+++ VERA/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('hello/', views.hello, name='hello')
+    path('hello/', views.hello, name='hello'),
+    path('' , views.task_list, name ="task_list"),
+    path('add/' , views.add_task, name ="add_task"),
+    path('delete/<int:task_id>/' ,views.add_task, name ="delete_task"),
+
 ]
--- vera/urls.py
+++ vera/urls.py
@@ -20,4 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('VERA.urls')),
+    #path('', include('todo_list.urls'))
 ]
