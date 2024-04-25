fe19ba81cfac44196e1dc4eb860328ebf149a5fd
veranyagaka <nyagakaver@gmail.com>
2024-04-25 16:53:59 +0300

    APIs

--- VERA/urls.py
+++ VERA/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('add/', views.add_task, name='add_task'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    #apis    
+    path('',views.home, name ='home'),
+
 ]
--- VERA/views.py
+++ VERA/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Task
+import requests
 
 def task_list(request):
     tasks = Task.objects.all()
@@ -19,3 +20,15 @@ def delete_task(request, task_id):
 
 def hello(request):
     return HttpResponse("Hello Vera! Welcome to Django Website")
+
+def home(request):
+  #using api
+  response = requests.get('https://api.github.com/events')
+  data = response.json()
+  result = data[0]["id"]
+  #result = data[0]["repo"]
+  #Response 2
+  response2 = requests.get('https://dog.ceo/api/breeds/image/random')
+  data2 = response2.json()
+  result2 = data2['message']
+  return render(request, 'templates/index.html', {'result': result, 'result2': result2})
